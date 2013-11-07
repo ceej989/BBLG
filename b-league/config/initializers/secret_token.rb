44f79978ecf65801df8371f371f71d2051c2f113
Aaron Stewart <aaron@binarypaean.com>
2013-11-07 10:45:17 -0800

    Blank rails project added

---
+++ b-league/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BLeague::Application.config.secret_key_base = '95fe0f3b4cfa28b5368932183a82b0a593c6de9625ff19b539ded976bcaed046c2869d3b4bff622530fad80502accb30b2deb266e0951206dca2ccc7c427f732'
