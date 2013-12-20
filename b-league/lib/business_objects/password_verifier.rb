e1e28997a14ed91af3c6da9469e5335901e93532
Aaron Stewart <aaron@binarypaean.com>
2013-12-19 17:14:17 -0800

    Renamed "Pverify" class to "PasswordVerifier". Updated "test:focused" rake task to also include business_objects folder in the search path.
    Modified PasswordVerifier so that all private methods who's names
    begin with "check_if" are automatically run by the "legit?" method,
    and have their results pushed onto the @errors list.

--- b-league/lib/business_objects/password_verifier.rb
+++ b-league/lib/business_objects/password_verifier.rb
@@ -1,4 +1,5 @@
-class Pverify
+class PasswordVerifier
+
 	def initialize(pw)
 		@password = pw
 		@errors = []
@@ -6,21 +7,23 @@ class Pverify
 
 
 	def legit?
-		istooshort
-		hasinteger
-		hascap
-
+		checks = self.private_methods.find_all {|m| m =~ /^check_if_.*/}
+		checks.each do |chk|
+			@errors << self.send(chk)
+		end
 		return @errors
 	end
 
 	private
-		def istooshort
+		def check_if_too_short
 			@errors.push("Password is too short (must be at least 7 characters long)")
 		end
-		def hasinteger
+
+		def check_if_has_integer
 			@errors.push("Password must contain at least one number")
 		end
-		def hascap
+		
+		def check_if_has_caps
 			@errors.push("Password must contain a capital letter")
 		end
 end
--- b-league/test/interactors/password_verifier_test.rb
+++ b-league/test/interactors/password_verifier_test.rb
@@ -1,22 +1,22 @@
 require 'minitest/spec'
 require 'minitest/autorun'
-require 'pverify'
+require 'password_verifier'
 
-describe Pverify do
+describe PasswordVerifier do
 	it "will reject passwords shorter than 7 characters long" do
-		p = Pverify.new("test")
+		p = PasswordVerifier.new("test")
 		assert_includes(p.legit?, "Password is too short (must be at least 7 characters long)") 
 	end
 	it "will reject passwords without a capital letter" do
-		p = Pverify.new("test")
+		p = PasswordVerifier.new("test")
 		assert_includes(p.legit?, "Password must contain a capital letter")
 	end
 	it "will reject passwords without an integer" do
-		p = Pverify.new("test")
+		p = PasswordVerifier.new("test")
 		assert_includes(p.legit?, "Password must contain at least one number")
 	end
 	it "will accept a strong password" do
-		p = Pverify.new("Test123")
+		p = PasswordVerifier.new("Test123")
 		assert_empty(p.legit?)
 	end
 end
